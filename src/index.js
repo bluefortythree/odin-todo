d7a6383c64e9d15df3100744d6c554a31c0c5eff
bluefortythree <117217594+bluefortythree@users.noreply.github.com>
2023-04-14 17:39:52 -0700

    add the project list implementation

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@
 let start = {
     new: (function () {
         let newProject = document.getElementById('new');
-        let form = document.getElementById('form');
-        let submit = document.getElementById('submit');
         newProject.addEventListener('click', () => {
             newProject.style.display = 'none';
             form.style.display = 'grid';
         })
+
+        let form = document.getElementById('form');
         form.addEventListener('submit',(event) => {
             event.preventDefault();
         })
+
+        let submit = document.getElementById('submit');
+        let list = [];
+        let view = document.getElementById('project-list');
         submit.addEventListener('click',() => {
             let title = document.getElementById('project-name').value;
             let description = document.getElementById('project-description').value;
@@ -18,8 +22,57 @@ let start = {
             let priority = document.getElementById('project-priority').value;
             let test = new Project(title, description, due, priority);
             console.log(test.info());
+            form.reset();
+            form.style.display = 'none';
+            newProject.style.display = 'grid';
+            list.push([title, description, due, priority]);
+            let container = document.createElement('div');
+            container.setAttribute('class', 'container');
+            let add = document.createElement('button');
+            let add2 = document.createElement('button');
+            let add3 = document.createElement('button');
+            add.setAttribute('class', 'add');
+            add2.setAttribute('class', 'add2');
+            add3.setAttribute('class', 'add3');
+            add3.setAttribute('id', list.length-1 + 'a');
+            add.setAttribute('id', list.length-1 + 'b');
+            add2.setAttribute('id', list.length-1 + 'c');
+            view.append(container);
+            container.append(add3, add, add2);
+            if(priority !== '') {
+                add3.innerText = priority;
+            } else {
+                add3.innerText = 'Normal';
+            }
+            add.innerText = title;
+            if(due !== '') {
+                add2.innerText = due;
+            } else {
+                add2.innerText = 'No Due Date';
+            }
+            return list;
+        });
+    })(),
+
+    view: (function () {
+        let newProject = document.getElementById('new');
+        let projectView = document.getElementById('project-view');
+        let add = document.getElementsByClassName('add');
+        let submit = document.getElementById('submit');
+        submit.addEventListener('click', () => {
+            for(i = 0; i < add.length; i++) {
+                add[i].addEventListener('click', () => {
+
+                // testing the highlight effect to know which project is being edited
+
+                let test = document.getElementById('0a');
+                    newProject.style.display = 'none';
+                    projectView.style.display = 'grid';
+                    test.style.backgroundColor = 'red';
+                })
+            }
         })
-    })()
+    })(),
 }
 
 
@@ -39,6 +92,6 @@ class Project {
             Priority: this.priority,
         }
     }
-
 }
+
 
